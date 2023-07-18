7dee24ac4ee6ff0913e41663cf08459e487af874
wesley-nunes <wesnmonteiro@gmail.com>
2023-07-18 12:45:30 -0300

    feat: Add the game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameOver = false;
+    public GameObject player;
+    public GameObject[] enemies = new GameObject[1];
+    void Update()
+    {
+        if (!gameOver)
+        {
+            int hp = player.GetComponent<Player>().hp;
+            if (hp == 0)
+            {
+                gameOver = true;
+            }
+            else
+            {
+                StartCoroutine(EnemiesAttackCheckRoutine());
+            }
+        }
+        else
+        {
+            StopAllCoroutines();
+            player.SetActive(false);
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].SetActive(false);
+            }
+        }    
+    }
+
+    IEnumerator EnemiesAttackCheckRoutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            bool playerHit = enemies[i].GetComponent<Enemy>().playerHit;
+            if(playerHit)
+            {
+                player.GetComponent<Player>().decreaseHp();
+                enemies[i].GetComponent<Enemy>().resetPlayerHitState();
+            }
+        }
+    }
+}
